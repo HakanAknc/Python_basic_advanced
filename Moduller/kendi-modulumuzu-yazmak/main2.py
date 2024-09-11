b588b3684a8c21ca1f43039d9e7512c44ab96171
Hakan Akinci <hakan062.2001@gmail.com>
2024-09-11 17:01:15 +0300

    kendi modulumuzu yazmak

---
+++ Moduller/kendi-modulumuzu-yazmak/main2.py
@@ -0,0 +1,12 @@
+"""
+Kendi Modüllerimizi Yazmak
+Bu konuda kendi modüllerimizi nasıl yazacağımızı öğrenmeye çalışacağız. Ancak bu derste Python dosyaları oluşturacağımız için kendi bilgisayarımızda konuyu anlatmaya çalışacağım. Uygulayacağımız adımlar şu şekilde;
+
+                1. Herhangi bir tane klasör oluşturuyoruz.
+                2. Modülümüz ve deneme Python dosyamız aynı dizinde olması gerektiği için 2 tane Python dosyasını da
+                bu klasör altında oluşturuyoruz.
+                3. modul1.py ve deneme.py dosyası oluşturuyoruz.
+                4. modul1.py dosyasının içine istediğimiz kadar fonksiyonu yazıyoruz.
+                5. Son olarak da deneme.py dosyasının içinde bu modul1.py modülünü kullanıyoruz.
+Peki yazdığımız bir modülü her yerden kullanmak için ne yapacağız ? Bunun için yazdığımız modul1.py dosyasının Python35/Lib klasörünün altına atmamız gerekiyor. Böylelikle bu modülü de math modülü gibi her dosyada kullanabiliriz.
+"""
