153a1e72d97c6860fc2d777d13e6db1e487f92a0
Hakan Akinci <hakan062.2001@gmail.com>
2024-10-11 08:33:53 +0300

    - r kipi - read -readline -readlines

--- Dosya Islemleri/dosya-okuma/main.py
+++ Dosya Islemleri/dosya-okuma/main.py
@@ -33,3 +33,69 @@ file = open("bilgiler.txt","r",encoding="utf-8") # Dosyayı okuma kipiyle açıy
 for i in file: # Tıpkı listeler gibi dosyanın her bir satırı üzerinde geziniyoruz.
     print(i)  # Her bir satırı ekrana basıyoruz.
 file.close()
+
+"""
+Burada her bir satırımız boşluklu yazıldı. 
+Bunun nedeni, hem her satır sonunda "\n" karakterinin olması hem de print fonksiyonun bir alt satıra geçmek için 
+boşluk bırakmasıdır. Bunu önlemek için varsayılan değer olarak "\n" karakteri alan *end* parametresine 
+kendimiz değer verebiliriz.
+"""
+file = open("bilgiler.txt","r",encoding= "utf-8") # Dosyayı okuma kipiyle açıyoruz. Türkçe karaktere dikkat.
+
+for i in file: # Tıpkı listeler gibi dosyanın her bir satırı üzerinde geziniyoruz.
+    print(i,end = "") # Her bir satırı ekrana basıyoruz. end parametresi \n yerine boşluk alacak.
+file.close()
+
+"""
+read() fonksiyonu
+read() fonksiyonu eğer içine hiçbir değer vermezsek bütün dosyamızı okuyacaktır.
+
+read() fonksiyonuna değer vererek belli bir kısmı okumayı bir sonraki dersimizde görmeye çalışsak daha doğru olur.
+"""
+
+file = open("bilgiler.txt","r",encoding="utf-8")
+
+icerik = file.read() 
+
+print("Dosya İçeriği:\n",icerik,sep ="")
+
+file.close()
+
+"""
+read() fonksiyonuyla bir dosyayı okuduğumuzda dosya imlecimiz dosyanın en sonuna gider ve read() fonksiyonu 2. okuma da artık boş string döner.
+"""
+
+file = open("bilgiler.txt","r",encoding="utf-8")
+
+icerik = file.read() 
+
+print("1. Okuma : Dosya İçeriği:\n",icerik,sep ="")
+
+icerik2 = file.read()
+
+
+print("2. Okuma : Dosya İçeriği:\n",icerik2,sep ="")
+
+file.close()
+
+"""
+readline() fonksiyonu
+readline() fonksiyonu her çağrıldığında dosyanın sadece bir satırını okur.
+ Her seferinde dosya imlecimiz (file) bir satır atlayarak devam eder.
+"""
+
+file = open("bilgiler.txt","r",encoding="utf-8")
+print(file.readline())
+
+
+print(file.readline()) # Okuyacak herhangi bir şey kalmayınca readline fonksiyonu boş string döner.
+file.close()
+
+"""
+readlines() fonksiyonu
+readlines() fonksiyonu dosyanın bütün satırları bir liste şeklinde döner.
+"""
+
+file = open("bilgiler.txt","r",encoding="utf-8")
+file.readlines()
+file.close()
