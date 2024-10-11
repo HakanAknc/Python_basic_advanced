1109a03302729a809d891f1ba23db38c98db6880
Hakan Akinci <hakan062.2001@gmail.com>
2024-10-11 08:33:03 +0300

    - r kipi (read) dosya okuma - for döngüüs ile dosya okuma - read() fonksiyonu eğer içine hiçbir değer vermezsek bütün dosyamızı okuyacaktır. - readline() fonksiyonu her çağrıldığında dosyanın sadece bir satırını okur. - readlines() fonksiyonu dosyanın bütün satırları bir liste şeklinde döner.

---
+++ Dosya Islemleri/dosya-acma-ve-yazma/main2.py
@@ -0,0 +1,8 @@
+# file  = open("nur.txt","w",encoding='utf-8')
+# file.write("Bünyamin benin kardeşim olmaya adaydır...com")   siler tekrar yazar 
+# print(file)
+
+
+file = open("üstad.txt","a",encoding='utf-8')
+file.write("Üstad Bediüzzaman Said Nursi Hazretleri...Barla")   # silmez üzerine tek yazar
+print(file)
---
+++ Dosya Islemleri/dosyalarda-kullanılan-fonksiyonlar/main.py
@@ -0,0 +1,3 @@
+file = open("hakan.txt", 'w', encoding='utf-8')
+print(file.write("Hakan Akıncı Dosya İşlemlerini öğreniyor."))
+file.close()
